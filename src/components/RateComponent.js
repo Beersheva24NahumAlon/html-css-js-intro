59654b4ef87ae1e9d27260d977971030b4e84f09
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-12 14:24:29 +0300

    made some refactoring in rating task

--- src/components/RateComponent.js
+++ src/components/RateComponent.js
@@ -11,6 +11,20 @@ export default class RateComponent {
 }
 
 function getRate(ratePercent, starsNumber) {
+    const starsNumbers = calculateStarsCounts(ratePercent, starsNumber)
+    const res = getRatingHTML(starsNumbers);
+    return res;
+}
+
+function getRatingHTML({fullStars, halfStars, emptyStars}) {
+    const fullStarHTML = "<i class='fa-solid fa-star'></i>";
+    const halfStarHTML = "<i class='fa fa-star-half-o'></i>";
+    const emptyStarHTML = "<i class='fa-regular fa-star'></i>";
+    const res = fullStarHTML.repeat(fullStars) + halfStarHTML.repeat(halfStars) + emptyStarHTML.repeat(emptyStars);
+    return res;
+}
+
+function calculateStarsCounts(ratePercent, starsNumber) {
     const rate = ratePercent / 100;
     const starsRating = starsNumber * rate;
     let fullStars = Math.floor(starsRating);
@@ -23,14 +37,5 @@ function getRate(ratePercent, starsNumber) {
         fullStars++;
     }
     const emptyStars = starsNumber - fullStars - halfStars;
-    const res = getRatingHTML(fullStars, halfStars, emptyStars);
-    return res;
-}
-
-function getRatingHTML(fullStars, halfStars, emptyStars) {
-    const fullStar = "<i class='fa-solid fa-star'></i>";
-    const halfStar = "<i class='fa fa-star-half-o'></i>";
-    const emptyStar = "<i class='fa-regular fa-star'></i>";
-    const res = fullStar.repeat(fullStars) + halfStar.repeat(halfStars) + emptyStar.repeat(emptyStars);
-    return res;
+    return {fullStars, halfStars, emptyStars};
 }
